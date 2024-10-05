e8da0d98f5b0489a132e21f2208ffb02fb3e3a4e
Bhanuja1999 <bhanusasanka1999@gmail.com>
2024-10-05 19:35:57 +0530

    update OrderRepository

--- src/main/java/com/example/order_management_api/repository/OrderRepository.java
+++ src/main/java/com/example/order_management_api/repository/OrderRepository.java
@@ -1,10 +1,18 @@
 package com.example.order_management_api.repository;
 
+import com.example.order_management_api.model.Client;
 import com.example.order_management_api.model.Order;
 import com.example.order_management_api.model.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 public interface OrderRepository extends JpaRepository<Order, String> {
     List<Order> findByStatus(OrderStatus status);
+    Optional<Order> findByReferenceNumberAndClient_Email(String referenceNumber, String email);
+    Page<Order> findByClient(Client client, Pageable pageable);
+
 }
